48576432a61eefc103b796ad5c29716bf7cc2a83
yakov sagamba <yakovsagamba301@gmail.com>
2025-10-16 16:07:17 -0400

    bunch of small changes

--- devoir_03_yakov_sagamba/src/main.cpp
+++ devoir_03_yakov_sagamba/src/main.cpp
@@ -3,6 +3,7 @@
 
 // Variables
 int machine = 0;
+int mappedmachine = 0;
 
 void setup() {
   Serial.begin(115200); // Initialisation de la communication sérielle
@@ -11,9 +12,9 @@ void setup() {
 void loop() {
 
     machine = analogRead(33);
+    mappedmachine = map(machine, 0, 4095, 0 , 1);
 
-    Serial.print(">pot1:");Serial.println(machine);
-    
+    Serial.println(mappedmachine);
 
   delay(100);
 
